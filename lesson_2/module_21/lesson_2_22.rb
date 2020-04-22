5a0000e98ea9f11c2254c646f5c19095648d8fc3
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-04-22 22:14:31 +0000

    Add file for RB101 > Lesson 2.22 exercise

--- lesson_2/module_21/lesson_2_22.rb
+++ lesson_2/module_21/lesson_2_22.rb
@@ -3,20 +3,22 @@ Link to exercise:
 https://launchschool.com/lessons/a0f3cd44/assignments/9e75343c
 =end
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(msg)
   Kernel.puts("=> #{msg}")
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_result(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
