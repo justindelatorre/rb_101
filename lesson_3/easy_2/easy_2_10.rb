9f59dbacb73c1a07ed29a1b11db33f62132cc0c1
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-04-25 15:27:17 +0000

    Add files for RB101 > Lesson 3 > Easy 2 exercises

---
+++ lesson_3/easy_2/easy_2_10.rb
@@ -0,0 +1,14 @@
+=begin
+Back in the stone age (before CSS) we used spaces to align things on the
+screen. If we had a 40 character wide table of Flintstone family members, how
+could we easily center that title above the table with spaces?
+=end
+
+title = 'Flintstone Family Members'
+
+title.center(40)
+
+=begin
+Explanation:
+https://ruby-doc.org/core-2.4.0/String.html#method-i-center
+=end
---
+++ lesson_3/easy_2/easy_2_9.rb
@@ -0,0 +1,10 @@
+=begin
+Write a one-liner to count the number of lower-case 't' characters in the
+following string:
+
+statement = "The Flintstones Rock!"
+=end
+
+statement = 'The Flintstones Rock!'
+
+puts statement.count('t')
