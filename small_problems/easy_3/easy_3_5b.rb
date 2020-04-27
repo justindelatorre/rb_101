da7853086fdce37e42fd4142f22c0c66e44dafae
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-04-27 18:45:56 +0000

    Link to recursion explanation

--- small_problems/easy_3/easy_3_5b.rb
+++ small_problems/easy_3/easy_3_5b.rb
@@ -2,6 +2,9 @@
 What if we wanted to generalize this method to a "power to the n" type method:
 cubed, to the 4th power, to the 5th, etc. How would we go about doing so while
 still using the multiply method?
+
+In-depth explanation of code:
+https://www.notion.so/jaydelatorre/Small-Problems-a5b9cdc336b043ed800a0c4a2944def3#3f6e4d24423045448b58ad1225e2cf81
 =end
 
 def multiply(a, b)
